2c45827c8ce971712de066cab5ff76faad5dbc8e
Zidni Mubarock <zidmubarock@gmail.com>
2013-08-11 02:47:12 +0700

    most beautiful routes I've ever made, I Love IT

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,36 @@
 AgileKitchen::Application.routes.draw do
-  devise_for :users
-  root :to => "application#root_page"
+	# All parts READY!!
+	member_action 		= [:show, :edit, :destroy]
+	collection_action = [:create, :index, :new]
+	concern(:story_recipeable){member{put :move}}
+	concern(:storizeable){resources(:story_recipes, :only => collection_action)}
+	concern :projectable do
+		resources :sprints, :story_tags , :only => collection_action
+		resources :project_members, :path => 'members', :only => collection_action
+		resources :stories, :concerns => :storizeable,  :only => collection_action
+	end
+
+	# LIGHT GREEN!! All tunnels opened (route started here)
+	devise_for :users
+	# -------------------------------------------
+	# start keep it sync
+	# A
+	scope :api do
+		resources :projects , :concerns => :projectable
+		resources :users
+		resources :sprints, :story_tags , :stories, :project_members
+		resources :story_recipeable , :concerns => :story_recipeable
+	end
+	# -------------------><----------------------
+	# B
+	resources :projects, :concerns => :projectable
+	resources :users
+	resources :sprints, :story_tags , :stories, :project_members
+	resources :story_recipeable , :concerns => :story_recipeable
+	# end keep it sync
+	# -------------------------------------------
+
+	# HIGHWAY
+	root :to => "base#dashboard"
+	# LIGHT RED!! All tunnels closed (route stoped here)
 end
