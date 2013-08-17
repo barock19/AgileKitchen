dea7c33ecd8a7a43648cf41591d5350253735fd7
Azhar Amir <cak.azharr@gmail.com>
2013-08-18 01:00:46 +0700

    feature user can add member to project

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -1,8 +1,14 @@
 class ProjectMember < ActiveRecord::Base
 	belongs_to :member, :class_name => 'User', :foreign_key => 'user_id'
 	belongs_to :project
+	validate :validate_user_and_project_existence
 	validates :user_id, :uniqueness => {:scope => :project_id,
 		:message => ->(error, attributes){
      "#{User.find(attributes[:value].to_i).email} already added to this project" 
      }}
+
+     def validate_user_and_project_existence
+     	errors.add :user_id, "Not Found" if User.where(:id => user_id).first.nil?
+    	errors.add :project_id, "Not Found" if Project.where(:id => project_id).first.nil?
+     end
 end
