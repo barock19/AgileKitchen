fc270ad3405ab3737ce0cdd403aa74953dc47f91
Zidni Mubarock <zidmubarock@gmail.com>
2013-08-18 14:02:04 +0700

    each story recipe has one project through story belongs_to it

--- app/models/story_recipe.rb
+++ app/models/story_recipe.rb
@@ -3,5 +3,6 @@ class StoryRecipe < ActiveRecord::Base
   belongs_to :initiator, :class_name => 'User', :foreign_key => 'initiator_id'
   belongs_to :actor, :class_name => 'User', :foreign_key => 'actor_id'
   belongs_to :story
+  has_one :project, :through => :story
   validates :description, :initiator_id, :story_id , :presence => true
 end
