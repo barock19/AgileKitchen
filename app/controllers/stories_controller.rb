ecc7d8ac9061897d88f4e6f5a44af1f003bd6e47
Zidni Mubarock <zidmubarock@gmail.com>
2013-08-18 12:58:39 +0700

    implement cancan with rails 4 Story can be created

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,11 @@
 class StoriesController <BaseController
-	before_filter :define_stories, :only => [:index]
-	before_filter :define_story, :only => [:show, :edit, :update, :destroy]
+	before_action :define_stories, :only => [:index]
+	before_action :define_story, :only => [:show, :edit, :update, :destroy]
+	before_action :define_new_story, :only => [:create]
+
+	load_and_authorize_resource :project
+	load_and_authorize_resource :story , :through => :project
+
 	def show
 		good_response @story
 	end
@@ -9,13 +14,18 @@ class StoriesController <BaseController
 	end
 	
 	def destroy
+		if @story.destroy
+			good_response :message => " story #{@story.id} successfully deleted"
+		else
+			bad_response @story.erros
+		end
 	end
 	
 	def new
 	end
 
 	def create
-		@story = Story.new(story_params.merge(:project_id => params[:project_id]))
+
 		if @story.save
 			good_response @story
 		else
@@ -28,6 +38,9 @@ class StoriesController <BaseController
 	end
 
 	private
+	def define_new_story
+		@story = Story.new(story_params.merge(:project_id => params[:project_id]))
+	end
 	def define_stories
 		@stories = Story.where("initiator_id = :u_id or actor_id = :u_id", u_id: current_user.id).try{|r| 
 			!params[:project_id].blank? ? r.where(:project_id => params[:project_id]) : r
